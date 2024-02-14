cb9a4b3a97803e0d66daaabd564262ce2aa29772
dangan7 <tuongandang98@gmail.com>
2024-02-14 14:26:25 +0200

    highlight by js, not yet filtering

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-document.getElementsByClassName
+// var bubbles = document.getElementById("bubs").children;
+var bubbles = document.querySelectorAll("#bubs p");
+
+for (var i = 0; i < bubbles.length; i++) {
+    bubbles[i].addEventListener("mouseover", highlightBubble, false);
+    bubbles[i].addEventListener("mouseout", resetBubble, false);
+}
+
+function highlightBubble(event) {
+    event.target.style.backgroundColor = "white";
+    event.target.style.transform = "scale(1.2)";
+}
+
+function resetBubble(event) {
+    event.target.style.backgroundColor = ""; 
+    event.target.style.transform = ""; 
+}
+
+
