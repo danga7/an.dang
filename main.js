0c9b8106bea0c2e1525ac3adda0d7fdcf6002fd8
dangan7 <tuongandang98@gmail.com>
2024-02-15 01:59:26 +0200

    added dropdown menu

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//dropdown menu on mobile
+
+// function menuDrop(){
+//     document.getElementById("dropContent").classList.toggle("show");
+// }
+
+const dropdownButt = document.getElementById("dropbutt");
+const dropdownMenu = document.getElementById("dropContent");
+const toggleCancel = document.getElementById("dropbutt");
+// toggle drop down menu on and off
+const toggleDropdown = function(){
+    dropdownMenu.classList.toggle("show");
+    toggleCancel.classList.toggle("cancel");
+}
+// close when any DOM element is clicked
+dropdownButt.addEventListener("click", function(e){
+    console.log("Button clicked");
+    e.stopPropagation();
+    toggleDropdown();
+});
+
+document.documentElement.addEventListener("click", function () {
+    if (dropdownMenu.classList.contains("show")) {
+      toggleDropdown();
+    }
+  });
+
 // var bubbles = document.getElementById("bubs").children;
 var bubbles = document.querySelectorAll("#bubs p");
 
@@ -15,5 +42,6 @@ function resetBubble(event) {
     event.target.style.backgroundColor = ""; 
     event.target.style.transform = ""; 
 }
+
 
 
